175028b9012909a5908ae59a46ca05f9a30abc9a
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-10-09 16:53:30 -0400

    Create firebase.config.ts

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC1qE64VO4Pj_NQp8ITMtxba1QtfWULWpY",
+  authDomain: "sp-fs-react-update-v18-efd50.firebaseapp.com",
+  projectId: "sp-fs-react-update-v18-efd50",
+  storageBucket: "sp-fs-react-update-v18-efd50.appspot.com",
+  messagingSenderId: "513851225210",
+  appId: "1:513851225210:web:409ba63ef5eb922acc37c3",
+  measurementId: "G-N6PETNY2Y1"
+};
